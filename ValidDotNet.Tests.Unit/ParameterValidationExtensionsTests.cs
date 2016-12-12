3ea043b7b39343b82ef6055a43c139538f9711ad
Sotirios Mantziaris <smantziaris@gmail.com>
2016-12-12 21:59:45 +0200

    initial commit

---
+++ ValidDotNet.Tests.Unit/ParameterValidationExtensionsTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+using FluentAssertions;
+using System.Linq;
+
+namespace ValidDotNet.Tests.Unit
+{
+    public class ParameterValidationExtensionsTests
+    {
+        #region Generic as class
+
+        [Fact]
+        public void GenericClass_ThrowIfNull()
+        {
+            Action act = () => ((object)null).ThrowIfNull("t");
+            var result = act.ShouldThrow<ArgumentNullException>();
+            result.Subject.First().Message.Should().Be("Parameter is null!\r\nParameter name: t");
+        }
+
+        [Fact]
+        public void GenericClass_customMessage_ThrowIfNull()
+        {
+            Action act = () => ((object)null).ThrowIfNull("t", "Message");
+            var result = act.ShouldThrow<ArgumentNullException>();
+            result.Subject.First().Message.Should().Be("Message\r\nParameter name: t");
+        }
+
+        #endregion
+
+        #region TimeSpan
+
+        [Fact]
+        public void TimeSpan_ThrowIfZero()
+        {
+            Action act = () => TimeSpan.Zero.ThrowIfZero("t");
+            var result = act.ShouldThrow<ArgumentException>();
+            result.Subject.First().Message.Should().Be("Parameter is zero!\r\nParameter name: t");
+        }
+
+        [Fact]
+        public void TimeSpan_CustomMessage_ThrowIfZero()
+        {
+            Action act = () => TimeSpan.Zero.ThrowIfZero("t", "Message");
+            var result = act.ShouldThrow<ArgumentException>();
+            result.Subject.First().Message.Should().Be("Message\r\nParameter name: t");
+        }
+
+        #endregion
+
+        #region String
+
+        [Fact]
+        public void String_ThrowIfNullOrWhitespace()
+        {
+            Action act = () => string.Empty.ThrowIfNullOrWhitespace("t");
+            var result = act.ShouldThrow<ArgumentException>();
+            result.Subject.First().Message.Should().Be("Parameter is null or whitespace!\r\nParameter name: t");
+        }
+
+        [Fact]
+        public void String_CustomMessage_ThrowIfNullOrWhitespace()
+        {
+            Action act = () => string.Empty.ThrowIfNullOrWhitespace("t", "Message");
+            var result = act.ShouldThrow<ArgumentException>();
+            result.Subject.First().Message.Should().Be("Message\r\nParameter name: t");
+        }
+
+        #endregion
+
+        #region int
+
+        [Fact]
+        public void Int_ThrowIfLessOrEqual()
+        {
+            Action act = () => 0.ThrowIfLessOrEqual("t", 0);
+            var result = act.ShouldThrow<ArgumentException>();
+            result.Subject.First().Message.Should().Be("Parameter is less or equal to 0!\r\nParameter name: t");
+        }
+
+        [Fact]
+        public void Int_CustomMessage_ThrowIfLessOrEqual()
+        {
+            Action act = () => 0.ThrowIfLessOrEqual("t", 0, "Message");
+            var result = act.ShouldThrow<ArgumentException>();
+            result.Subject.First().Message.Should().Be("Message\r\nParameter name: t");
+        }
+
+        #endregion
+
+        #region CancellationToken
+
+        [Fact]
+        public void CancellationToken_ThrowIfLessOrEqual()
+        {
+            Action act = () => CancellationToken.None.ThrowIfNone("t");
+            var result = act.ShouldThrow<ArgumentException>();
+            result.Subject.First().Message.Should().Be("Parameter is none!\r\nParameter name: t");
+        }
+
+        [Fact]
+        public void CancellationToken_CustomMessage_ThrowIfLessOrEqual()
+        {
+            Action act = () => CancellationToken.None.ThrowIfNone("t", "Message");
+            var result = act.ShouldThrow<ArgumentException>();
+            result.Subject.First().Message.Should().Be("Message\r\nParameter name: t");
+        }
+
+        #endregion
+    }
+}
---
+++ ValidDotNet/ParameterValidationExtensions.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading;
+
+namespace ValidDotNet
+{
+    public static class ParameterValidationExtensions
+    {
+        #region Generic of class
+
+        /// <summary>
+        /// Handling the null check and throw a ArgumentNullException if null
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter</typeparam>
+        /// <param name="parameter">the actual parameter</param>
+        /// <param name="parameterName">the name of the parameter</param>
+        /// <param name="message">a optional message instead of the default</param>
+        public static void ThrowIfNull<T>(this T parameter, string parameterName, string message = null) where T : class
+        {
+            if (parameter == null)
+            {
+                throw new ArgumentNullException(parameterName, message ?? "Parameter is null!");
+            }
+        }
+
+        /// <summary>
+        /// Handling the null and empty check of an array and throw a 
+        /// ArgumentNullException if null and ArgumentException if empty
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter</typeparam>
+        /// <param name="parameter">the actual parameter array</param>
+        /// <param name="parameterName">the name of the parameter</param>
+        /// <param name="message">a optional message instead of the default</param>
+        public static void ThrowIfNullOrEmpty<T>(this T[] parameter, string parameterName, string message = null) where T : class
+        {
+            if (parameter == null)
+            {
+                throw new ArgumentNullException(parameterName, message ?? "Parameter is null!");
+            }
+
+            if (parameter.Length == 0)
+            {
+                throw new ArgumentException("Parameter is empty!", parameterName);
+            }
+        }
+
+        #endregion
+
+        #region TimeSpan
+
+        /// <summary>
+        /// Handling the zero check and throw a ArgumentException if TimeSpan.Zero
+        /// </summary>
+        /// <param name="parameter">the actual parameter</param>
+        /// <param name="parameterName">the name of the parameter</param>
+        /// <param name="message">a optional message instead of the default</param>
+        public static void ThrowIfZero(this TimeSpan parameter, string parameterName, string message = null)
+        {
+            if (parameter == TimeSpan.Zero)
+            {
+                throw new ArgumentException(message ?? "Parameter is zero!", parameterName);
+            }
+        }
+
+        #endregion
+
+        #region String
+
+        /// <summary>
+        /// Handling the null or whitespace check and throw a ArgumentException
+        /// </summary>
+        /// <param name="parameter">the actual parameter</param>
+        /// <param name="parameterName">the name of the parameter</param>
+        /// <param name="message">a optional message instead of the default</param>
+        public static void ThrowIfNullOrWhitespace(this string parameter, string parameterName, string message = null)
+        {
+            if (string.IsNullOrWhiteSpace(parameter))
+            {
+                throw new ArgumentException(message ?? "Parameter is null or whitespace!", parameterName);
+            }
+        }
+
+        #endregion
+
+        #region int 
+
+        /// <summary>
+        /// Handling the integer is negative check null or whitespace check and throw a ArgumentException
+        /// </summary>
+        /// <param name="parameter">the actual parameter</param>
+        /// <param name="parameterName">the name of the parameter</param>
+        /// <param name="value">the value to check</param>
+        /// <param name="message">a optional message instead of the default</param>
+        public static void ThrowIfLessOrEqual(this int parameter, string parameterName, int value, string message = null)
+        {
+            if (parameter <= value)
+            {
+                throw new ArgumentException(message ?? string.Format("Parameter is less or equal to {0}!", value), parameterName);
+            }
+        }
+
+        #endregion
+
+        #region CancellationToken
+
+        /// <summary>
+        /// Handling the none check and throw a ArgumentException if CancellationToken.None
+        /// </summary>
+        /// <param name="parameter">the actual parameter</param>
+        /// <param name="parameterName">the name of the parameter</param>
+        /// <param name="message">a optional message instead of the default</param>
+        public static void ThrowIfNone(this CancellationToken parameter, string parameterName, string message = null)
+        {
+            if (parameter == CancellationToken.None)
+            {
+                throw new ArgumentException(message ?? "Parameter is none!", parameterName);
+            }
+        }
+
+        #endregion
+    }
+}
