ce3507d58961226e29ad57fdb3f93c75fe1a2996
Sotirios Mantziaris <smantziaris@gmail.com>
2019-02-11 21:07:45 +0200

    Moved to netstandart 2.0

--- ValidDotNet/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ValidDotNet")]
-[assembly: AssemblyTrademark("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("bb125a28-76dc-405a-a3fd-aec22f4cd370")]
